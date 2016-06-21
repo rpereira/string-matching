472e1f47976e81d66a9109460feeae630c999fe4
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-06-21 11:25:56 +0100

    Add constructor to build and array of Suffix objects

--- src/main/java/SuffixArray.java
+++ src/main/java/SuffixArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SuffixArray {
 
   /**
@@ -5,10 +7,34 @@ public class SuffixArray {
    */
   private Suffix[] suffixes;
 
-  // TODO
-  public SuffixArray(String text) { }
+  /**
+   * Build an array of Suffix objects for the given text String, and sort them.
+   *
+   * The purpose of sorting is that index(i) just returns the index associated
+   * with suffixes[i].
+   */
+  public SuffixArray(String text) {
+    int textLength = text.length();
 
-  private static class Suffix {
+    suffixes = new Suffix[textLength];
+    for (int i = 0; i < textLength; i++) {
+      suffixes[i] = new Suffix(text, i);
+    }
+
+    Arrays.sort(suffixes);
+  }
+
+  /**
+   * This nested class represents a suffix of a text string.
+   *
+   * A Suffix has two instance variables:
+   *   - a String reference to the text string
+   *   - an int index of its first charecter
+   *
+   * It also provides four utility methods, namely length(), charAt(i),
+   * toString(), and compareTo().
+   */
+  private static class Suffix implements Comparable<Suffix> {
     /**
      * Reference to text string.
      */
@@ -30,6 +56,24 @@ public class SuffixArray {
 
     private char charAt(int i) {
       return text.charAt(index + i);
+    }
+
+    public int compareTo(Suffix that) {
+      if (this == that) {
+        return 0;
+      }
+
+      int length = Math.min(this.length(), that.length());
+      for (int i = 0; i < length; i++) {
+        if (this.charAt(i) < that.charAt(i)) {
+          return -1;
+        }
+        if (this.charAt(i) > that.charAt(i)) {
+          return 1;
+        }
+      }
+
+      return this.length() - that.length();
     }
 
     public String toString() {
--- src/test/java/SuffixArrayTest.java
+++ src/test/java/SuffixArrayTest.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 public class SuffixArrayTest {
+  @Test
+  public void testSuffixArray() {
+    String text = "ABRACADABRA!";
+    SuffixArray suffix = new SuffixArray(text);
+
+    assertNotNull("should not be null", suffix);
+  }
 }
